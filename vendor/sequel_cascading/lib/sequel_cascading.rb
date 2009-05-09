bf6acd37abdb6cdfdc0186dbf88a9d92edbb415c
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-05-09 20:19:08 -0300

    Update sequel_timestamped and sequel_cascading so they don't throw deprecation warnings

--- vendor/sequel_cascading/lib/sequel_cascading.rb
+++ vendor/sequel_cascading/lib/sequel_cascading.rb
@@ -3,13 +3,13 @@ module Sequel
     module Cascading
       def self.apply(model, options = {})
         Array(options[:destroy]).each do |assoc|
-          model.instance_eval "before_destroy { #{assoc}_dataset.destroy }"
+          model.instance_eval "def before_destroy; super; #{assoc}_dataset.destroy; end"
         end
         Array(options[:nullify]).each do |assoc|
-          model.instance_eval "before_destroy { remove_all_#{assoc} }"
+          model.instance_eval "def before_destroy; super; remove_all_#{assoc}; end"
         end
         Array(options[:restrict]).each do |assoc|
-          model.instance_eval "before_destroy { raise Error::InvalidOperation, 'Delete would orphan associated #{assoc}' unless #{assoc}_dataset.empty? }"
+          model.instance_eval "def before_destroy; super; raise Error::InvalidOperation, 'Delete would orphan associated #{assoc}' unless #{assoc}_dataset.empty?; end"
         end
       end
     end
--- vendor/sequel_timestamped/lib/sequel_timestamped.rb
+++ vendor/sequel_timestamped/lib/sequel_timestamped.rb
@@ -3,8 +3,8 @@ module Sequel
     module Timestamped
       def self.apply(model, options = {})
         using = options[:using] || :localtime
-        model.class_eval "before_create { self.created_at = Time.now.#{using} if self.created_at.nil? }" if model.columns.include?(:created_at)
-        model.class_eval "before_save { self.updated_at = Time.now.#{using} }" if model.columns.include?(:updated_at)
+        model.class_eval "def before_create; super; self.created_at = Time.now.#{using} if self.created_at.nil?; end" if model.columns.include?(:created_at)
+        model.class_eval "def before_save; super; self.updated_at = Time.now.#{using}; end" if model.columns.include?(:updated_at)
       end
     end
   end
