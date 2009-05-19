c4779daad7155b7b2c2183a8ccf0b6ebba865433
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-05-19 06:50:46 -0300

    More tests for Configurator

--- test/unit/configurator_test.rb
+++ test/unit/configurator_test.rb
@@ -25,4 +25,27 @@ class TestConfigurator < Integrity::TestCase
     @config.build_path = "/foo/bar"
     Bob.directory.should == "/foo/bar"
   end
+
+  test "The database connects automatically to whatever is set in database_uri" do
+    @config.database_uri = "sqlite::memory:"
+    @config.database.is_a?(Sequel::Database).should be(true)
+  end
+
+  test "The logger is instantiated automatically if unavailable" do
+    @config.log_file = "/dev/null"
+    @config.logger.is_a?(Integrity::Logger).should be(true)
+  end
+
+  context "Available options" do
+    def self.it_understands(*options)
+      options.each do |option|
+        test "understands #{option}" do
+          @config.respond_to?(option).should be(true)
+          @config.respond_to?("#{option}=").should be(true)
+        end
+      end
+    end
+
+    it_understands :log_file, :logger, :database_uri, :database, :build_path
+  end
 end
